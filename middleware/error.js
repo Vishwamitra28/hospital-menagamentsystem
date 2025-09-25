cd364f5cc95fd707c62f765ae97752f5141da1bd
Vishwamitra28 <vishwamitra835@gmail.com>
2025-09-25 22:06:24 +0530

    Add files via upload

---
+++ middleware/error.js
@@ -0,0 +1,38 @@
+class ErrorHandler extends Error {
+  constructor(message, statusCode) {
+    super(message);
+    this.statusCode = statusCode;
+  }
+}
+
+export const errorMiddleware = (err, req, res, next) => {
+  err.message = err.message || "Internal Server Error";
+  err.statusCode = err.statusCode || 500;
+
+  if (err.code === 11000) {
+    const message = `Duplicate ${Object.keys(err.keyValue)} entered`;
+    err = new ErrorHandler(message, 400);
+  } else if (err.name === "JsonWebTokenError") {
+    const message = "Invalid JSON Web Token. Please try again!";
+    err = new ErrorHandler(message, 400);
+  } else if (err.name === "TokenExpiredError") {
+    const message = "JSON Web Token has expired. Please try again!";
+    err = new ErrorHandler(message, 400);
+  } else if (err.name === "CastError") {
+    const message = `Invalid ${err.path}`;
+    err = new ErrorHandler(message, 400);
+  }
+
+  const errorMessage = err.errors
+    ? Object.values(err.errors)
+        .map((error) => error.message)
+        .join(" ")
+    : err.message;
+
+  return res.status(err.statusCode).json({
+    success: false,
+    message: errorMessage,
+  });
+};
+
+export default ErrorHandler;
