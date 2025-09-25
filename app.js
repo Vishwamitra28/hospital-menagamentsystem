e075bc5623aee1d38d1d57773d282513360a1de6
Vishwamitra28 <vishwamitra835@gmail.com>
2025-09-25 22:28:29 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,45 @@
+import express from "express";
+import { dbConnection  } from "./database/dbConnection.js";
+import { config } from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import fileUpload from "express-fileupload";
+import { errorMiddleware } from "./middlewares/error.js";
+import messageRouter from "./router/messageRouter.js";
+import userRouter from "./router/userRouter.js";
+import appointmentRouter from "./router/appointmentRouter.js";
+import dotenv from 'dotenv';
+dotenv.config();
+const app = express();
+
+// Debug: Log MONGO_URI to verify it's loaded
+console.log("MONGO_URI:", process.env.MONGO_URI);
+
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL_ONE, process.env.FRONTEND_URL_TWO],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  })
+);
+
+app.use("/api/v1/message", messageRouter);
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/appointment", appointmentRouter);
+
+dbConnection();
+
+app.use(errorMiddleware);
+
+export default app;
